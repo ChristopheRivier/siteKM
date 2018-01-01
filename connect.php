1f236559c38b2f803e134495647c56abc50c5149
Christophe R <chrivier@gmail.com>
2018-01-01 22:39:27 +0100

    rr

---
+++ connect.php
@@ -0,0 +1,30 @@
+<?php
+
+    $username = "crr";
+
+    $password = "salut";
+
+    if( isset($_REQUEST['login']) ){
+
+
+        if($_REQUEST['login'] == $username ){ // Si les infos correspondent...
+
+            session_start();
+
+            $_SESSION['user'] = $username;
+
+            echo "Success";
+
+        }
+
+        else{ // Sinon
+
+            echo "Failed";
+
+        }
+
+
+    }
+
+
+?>
--- util.php
+++ util.php
@@ -17,11 +17,9 @@ function connectionbase()
 }
 function ecritEntete()
 {
-echo'<?xml version="1.0" encoding="iso-8859-1"?>'."\n";
-echo'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"'."\n";
-echo'    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'."\n";
-echo'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">'."\n"."\n";
-echo'   <head>'."\n";
+echo '<!DOCTYPE html>';
+echo '<html>'."\n"."\n";
+echo '   <head>'."\n";
 echo ' <meta charset="utf-8" />'."\n";
 echo ' <link rel="stylesheet" href="km.css"/>'."\n";
 echo ' <title>Saisie des km</title>'."\n";
@@ -44,11 +42,17 @@ echo '
           </ul>
         </li>
         <li><a href="#">Affichage</a>
-        <ul>
-          <li><a href="aff_sortie.php">Sortie</a></li>
-          <li><a href="aff_velo.php">Vélo</a></li>
-          <li><a href="aff_stat.php">Statistic</a></li>
-        </ul>
+          <ul>
+            <li><a href="aff_sortie.php">Sortie</a></li>
+            <li><a href="aff_velo.php">Vélo</a></li>
+            <li><a href="aff_stat.php">Statistic</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="profile.php">Membre</a>
+          <ul>
+              <li><a href="connexion.php">Login</a></li>
+          </ul>
         </li>
     </ul>
 </nav>
